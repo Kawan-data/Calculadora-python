c442e057c56edce4b742de5b3afe9264151f9848
Kawan Filipe <kawannunesramos@gmail.com>
2026-06-03 11:27:50 -0300

    Primeiro commit calculadora

---
+++ calculadora_python.sh
@@ -0,0 +1,14 @@
+#Calculadora com 4 operações
+n1 = float(input("Digite o primeiro número: "))
+n2 = float(input("Digite o segundo número: "))
+op = input("insira a operação:")
+if op == "+":
+        print(n1, op, n2, "=", n1 + n2)
+elif op == "-":
+        print(n1, op, n2, "=", n1 - n2)
+elif op == "*":
+        print(n1, op, n2, "=", n1 * n2)
+elif op == "/":
+        print(n1, op, n2, "=", n1 / n2)
+else:
+        print("Operação inválida")
